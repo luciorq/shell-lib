db9eb3cb280d00e459792f1a42f63147a8a7ff7c
luciorq <luciorqueiroz@gmail.com>
2022-02-24 16:48:57 -0500

    feat: add language specific support for base dirs

--- force-xdg-basedirs.sh
+++ force-xdg-basedirs.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/env bash
 
 # Unfortunately some tools will not use XDG directories unless these are set
 #
@@ -87,8 +87,35 @@ export WEECHAT_HOME="$XDG_CONFIG_HOME/weechat"
 
 # wget
 export WGETRC="$XDG_CONFIG_HOME/wgetrc"
+alias wget='wget --hsts-file="$XDG_CACHE_HOME/wget-hsts"'
 
 # java openjdk
 export _JAVA_OPTIONS=-Djava.util.prefs.userRoot="$XDG_CONFIG_HOME"/java
+
+# python / jupyter
+export IPYTHONDIR="$XDG_CONFIG_HOME"/jupyter
+export JUPYTER_CONFIG_DIR="$XDG_CONFIG_HOME"/jupyter
+export PYLINTHOME="$XDG_CACHE_HOME"/pylint
+export PYTHON_EGG_CACHE="$XDG_CACHE_HOME"/python-eggs
+
+# Ruby gem
+export GEM_HOME="$XDG_DATA_HOME"/gem
+export GEM_SPEC_CACHE="$XDG_CACHE_HOME"/gem
+
+# Rust
+export RUSTUP_HOME="$XDG_DATA_HOME"/rustup
+
+# Golang
+export GOPATH="$XDG_DATA_HOME"/go
+
+# Make R respect XDG base dirs spec
+export R_ENVIRON_USER="${XDG_CONFIG_HOME}/R/Renviron"
+export R_PROFILE_USER="${XDG_CONFIG_HOME}/R/Rprofile"
+export R_HISTFILE="${XDG_DATA_HOME}/R/Rhistory"
+# GNU history respect XDG base dirs spec
+export HISTFILE="${XDG_DATA_HOME}/bash/history"
+
+# Starship prompt configuration path
+export STARSHIP_CONFIG="${XDG_CONFIG_HOME}/starship/config.toml"
 
 # luciorq extensions END
