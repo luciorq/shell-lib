b4410e6ea12b4c4ddc829cef13503416aceaaac3
luciorq <luciorqueiroz@gmail.com>
2022-03-22 16:48:45 -0400

    refactor: Separate ruby pased YAML parser

--- parse_yaml.sh
+++ parse_yaml.sh
@@ -2,26 +2,37 @@
 
 # Extract variables from YAML a file
 function parse_yaml () {
-  local ruby_bin ruby_script;
+  local yq_bin;
+  local ruby_bin;
+  local method;
+  local args_arr;
+  yq_bin="$(which_bin 'yq')";
+  ruby_bin="$(which_bin 'ruby')";
+  declare -a args_arr=($@);
+  method='';
+  if [[ -n ${yq_bin} ]]; then
+    method='yq';
+  elif [[ -n ${ruby_bin} ]]; then
+    method='ruby';
+  fi
+  case "${method}" in
+    yq)     __parse_yaml_yq ${arg_arr[@]};;
+    ruby)   __parse_yaml_ruby ${args_arr[@]};;
+    *) builtin echo >&2 -ne "No method available for parsing YAML.\n"; return 1;;
+  esac
+}
+
+function __parse_yaml_ruby () {
+  local ruby_bin;
+  local ruby_script;
   local yaml_path;
   local args_arr;
   local yaml_levels levels;
-  local i args_range;
-  declare -a yaml_levels;
   yaml_path="$1";
-
-  args_arr=($@);
-  args_seq=( $(seq ${#args_arr[@]}) );
- 
-  for i in ${args_seq[@]}; do
-    if [[ -n "${args_arr[${i}]}" ]]; then
-      yaml_levels[ $(( ${i} - 1 )) ]=${args_arr[${i}]};
-    fi
-  done
-  # TODO luciorq Add check for MacOS or at least having
-  # + ruby installed, or try python or yq if ruby is not
-  # + available.
+  declare -a args_arr=($@);
+  declare -a yaml_levels=(${args_arr[@]:1})
   ruby_bin="$(which_bin 'ruby')";
+
   ruby_script="var_res=YAML::load(open(ARGV.first).read)";
   for levels in ${yaml_levels[@]}; do
     ruby_script="${ruby_script}['${levels}']";
@@ -31,3 +42,8 @@ function parse_yaml () {
   "${ruby_bin}" -ryaml -e "${ruby_script}" "${yaml_path}";
 }
 
+function __parse_yaml_yq () {
+  local yq_bin;
+  yq_bin="$(which_bin 'yq')";
+  echo "${yq_bin}";
+}
