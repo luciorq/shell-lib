34e33ec94cc2a97f7068d948cde5050a54e357ef
luciorq <luciorqueiroz@gmail.com>
2022-04-11 20:22:41 -0400

    feat: check home usage on linux

--- server-monitoring.sh
+++ server-monitoring.sh
@@ -9,10 +9,31 @@ function check_temps () {
   # sensors
 }
 
-
+# TODO add check for dir is ZFS and print real(compressed usage)
+# + and quota, if available;
+# Print Space used at home dir with time of last modification
+function check_home_usage () {
+  local home_path_arr;
+  local user_home;
+  if [[ -f /etc/passwd ]]; then
+    declare -a home_path_arr=( $(cat /etc/passwd \
+      | grep -v "/nologin" \
+      | grep -v "/false" \
+      | grep -v "/sync" \
+      | grep -v "^rstudio-server" \
+      | grep -v "^slurm" \
+      | grep -v "^_" \
+      | grep -v "^#" \
+      | cut -d":" -f 6
+    ) )
+  fi
+  for user_home in ${home_path_arr[@]}; do
+    sudo du -shL --time "${user_home}";
+  done
+}
 
 # ======================
-# Inventory function
+# Hardware Inventory functions
 function __list_network_devices () {
   sudo lshw -class network -short
 }
@@ -30,4 +51,4 @@ function n_threads () {
 }
 
 
-# 
+#
