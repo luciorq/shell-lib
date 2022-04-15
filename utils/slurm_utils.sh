8daa11dc14fe79cfbdf37b3bfb4807d5a4fb9ac3
luciorq <luciorqueiroz@gmail.com>
2022-04-15 17:47:36 -0400

    feat: job scheduler deploy and monotoring tools

---
+++ utils/slurm_utils.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Retrieve Running or complete job details
+function slurm_check_job () {
+  local job_id;
+  declare -a job_id=($@);
+  SACCT_FORMAT="JobID%20,JobName,User,Partition,NodeList,Elapsed,State,ExitCode,MaxRSS,AllocTRES%32" \
+    acct -j ${job_id[@]};
+}
+
+# Check partitions details
+function slurm_check_partitions () {
+  sinfo -N -o "%25N %5c %10m %15l %25R"
+}
+
