b23bcc32cc58a3be18937063c83a8b90b52cf61e
luciorq <luciorqueiroz@gmail.com>
2022-04-18 11:31:20 -0400

    refactor: returned cat_ccolor to bat as default

--- color_cmds.sh
+++ color_cmds.sh
@@ -2,26 +2,37 @@
 
 function type_color () {
   local fn_args;
-  declare -a fn_args=($@);
+  declare -a fn_args=(${@});
   local bat_bin="$(require 'bat')";
   builtin type ${fn_args[@]} \
     | "${bat_bin}" -l 'bash' -p --paging 'never' --theme 'gruvbox-dark';
 }
 
 function cat_color () {
-  local fn_args;
-  declare -a fn_args=($@);
+  local fn_args file_ext;
+  declare -a fn_args=(${@});
   local bat_bin="$(require 'bat')";
   local cat_bin="$(require 'cat')";
-  "${cat_bin}" ${fn_args[@]} \
-    | "${bat_bin}" -p --paging 'never' --theme 'gruvbox-dark';
+
+  file_ext="$(basename ${1})";
+  file_ext="${file_ext##*.}";
+  if [[ -z ${file_ext} ]]; then
+    file_ext="sh";
+  fi
+  # "${cat_bin}" ${fn_args[@]}
+  "${bat_bin}" -p --paging 'never' \
+    --theme 'gruvbox-dark' \
+    --color always \
+    ${fn_args[@]};
 }
 
 # This function only works correctly if the terminal is
 # + using 24-bits truecolors
 function print_truecolor_scale () {
+  local awk_bin;
+  awk_bin="$(require 'awk')";
   builtin echo -ne "'COLORTERM' is '${COLORTERM}' in '${TERM}'";
-  awk 'BEGIN{
+  "${awk_bin}" 'BEGIN{
     s="/\\/\\/\\/\\/\\"; s=s s s s s s s s;
     for (colnum = 0; colnum<77; colnum++) {
       r = 255-(colnum*255/76);
