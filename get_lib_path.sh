42b5364c17e22603ce1170965751650fb16d6024
luciorq <luciorqueiroz@gmail.com>
2022-04-21 18:19:13 -0400

    feat: add lib path find heuristic

---
+++ get_lib_path.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+function get_lib_path () {
+  local lib_path;
+  local lib_name;
+  lib_name="${1}";
+  if [ -d "${HOME}/projects/${lib_name}" ]; then
+    lib_path="${HOME}/projects/${lib_name}";
+  elif [ -d "${XDG_LIB_HOME}/${lib_name}" ]; then
+    lib_path="${XDG_LIB_HOME}/${lib_name}";
+  elif [ -d "${HOME}/.local/lib/${lib_name}" ]; then
+    lib_path="${HOME}/.local/lib/${lib_name}";
+  else
+    builtin echo >&2 -ne "Lib path '${lib_name}' not available\n";
+    builtin return 1;
+  fi
+  builtin echo -ne "${lib_path}";
+  return 0;
+}
