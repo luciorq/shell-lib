c01a16f2057743eee65149eb0542600dfdacc561
luciorq <luciorqueiroz@gmail.com>
2022-05-27 08:58:08 -0400

    refactor: enhance robustness for remote kitty kittens

--- icat.sh
+++ icat.sh
@@ -4,6 +4,10 @@
 # + if kitty API is available use
 # + icat kitten
 function icat () {
-  kitty +kitten icat "$@";
-
+  local bash_bin;
+  local kitty_bin;
+  bash_bin="$(which_bin 'bash')";
+  kitty_bin="$(which_bin 'kitty')";
+  "${bash_bin}" "${kitty_bin}" +kitten icat "${@}";
+  return 0;
 }
--- ssh_fun.sh
+++ ssh_fun.sh
@@ -5,16 +5,21 @@ function ssh_fun () {
   local ssh_bin;
   local chmod_bin;
   ssh_bin="$(require 'ssh' '-V')";
+  chmod_bin="$(require 'chmod')";
   set -x;
   if [[ ${_KITTY_SSH} == true ]]; then
     TERM='xterm-256color' "${ssh_bin}" "${@:1}";
   elif [[ ${TERM}  == xterm-kitty ]]; then
-    if [[ -f ${HOME}/.local/share/kitty-ssh-kitten/kitty/bin/kitty ]]; then
-      chmod_bin="$(require 'chmod')";
-      "${chmod_bin}" a+x "${HOME}/.local/share/kitty-ssh-kitten/kitty/bin/kitty";
-      "${chmod_bin}" -R a+x "${HOME}/.local/share/kitty-ssh-kitten";
+    if [[ -d ${HOME}/.local/share/kitty-ssh-kitten ]]; then
       "${chmod_bin}" a+x \
-        "${HOME}/.local/share/kitty-ssh-kitten/shell-integration/bash/kitty.bash"
+        "${HOME}/.local/share/kitty-ssh-kitten/kitty/bin/kitty" \
+        || builtin echo -ne '';
+      "${chmod_bin}" -R a+x \
+        "${HOME}/.local/share/kitty-ssh-kitten" \
+        || builtin echo -ne '';
+      "${chmod_bin}" a+x \
+        "${HOME}/.local/share/kitty-ssh-kitten/shell-integration/bash/kitty.bash" \
+        || builtin echo -ne '';
     fi
     kitty +kitten ssh "${@:1}";
   else
