97778e9c0346c048410b747c8a13fe8787a33e2b
luciorq <luciorqueiroz@gmail.com>
2022-08-08 19:43:22 -0400

    feat(rstats): create function to install packages

--- rstats_lib.sh
+++ rstats_lib.sh
@@ -23,3 +23,33 @@ function rstats::boostrap_quarto_install () {
   "${quarto_bin}" install tool chromium;
   return 0;
 }
+
+# Install R Packages
+rstats::install_pkg () {
+  local _usage="Usage: ${0} <> [cran|gh|local|bioc]";
+  unset _usage;
+  local pkg_name;
+  local pkg_type
+  local r_bin;
+  local num_threads;
+  local install_str;
+  local script_str;
+  pkg_name="${1}";
+  pkg_type="${2:-cran}";
+  r_bin="$(require 'R')";
+  case ${pkg_type} in
+    cran)      install_str='install.packages'           ;;
+    gh)        install_str='remotes::install_github'    ;;
+    local)     install_str='remotes::install_local'     ;;
+    bioc*)     install_str='BiocManager::install'       ;;
+    *)
+      builtin echo >&2 -ne "'${pkg_type}' not available as a Source.\n";
+      return 1;
+    ;;
+  esac
+  num_threads="$(get_nthreads 24)";
+  script_str="if(isFALSE(base::requireNamespace('${pkg_name}',quietly=TRUE))){${install_str}('${pkg_name}',Ncpus=${num_threads})}";
+  "${r_bin}" -q -s -e \
+    "${script_str}";
+  return 0;
+}
