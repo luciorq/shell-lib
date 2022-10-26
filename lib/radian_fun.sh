97c1099d6e513ec10215ef3f5757112562a3c34a
luciorq <luciorqueiroz@gmail.com>
2022-10-26 13:52:32 -0400

    refactor: call radian as module instead of executable

--- lib/radian_fun.sh
+++ lib/radian_fun.sh
@@ -3,14 +3,16 @@
 # Run Radian R prompt with some defaults
 function radian_fun () {
   local r_bin;
-  local radian_bin;
+  local py_bin;
+  # local radian_bin;
   r_bin="$(which_bin 'R')";
-  radian_bin="$(which_bin 'radian')";
-  if [[ -z ${radian_bin} ]];then
-    exit_fun "'radian' is not available on '${PATH}'";
-    return 1;
-  fi
-  _IS_RADIAN=true "${radian_bin}" \
+  py_bin="$(which_bin 'python3')";
+  # radian_bin="$(which_bin 'radian')";
+  # if [[ -z ${radian_bin} ]];then
+  #  exit_fun "'radian' is not available on '${PATH}'";
+  #  return 1;
+  # fi
+  _IS_RADIAN=true "${py_bin}" -m radian \
     --r-binary="${r_bin}" \
     --profile="${HOME}/.config/radian/profile" \
     --global-history --quiet;
