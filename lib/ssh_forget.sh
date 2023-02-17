7e06774bd08f365a0cfc3cb10ecf1e212c6ad1cf
luciorq <luciorqueiroz@gmail.com>
2023-02-17 16:36:16 -0500

    feat: create ssh_forget

---
+++ lib/ssh_forget.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+function ssh_forget () {
+  local keygen_bin;
+  local rm_bin;
+  local _host_name;
+  rm_bin="$(which_bin 'rm')";
+  keygen_bin="$(require 'ssh-keygen')";
+
+  if [[ -z ${} ]]; then
+    exit_fun '`ssh-keygen` is not available on PATH';
+    return 1;
+  fi
+
+  for _host_name in "${@}"; do
+    "${keygen_bin}" -R "${_host_name}";
+  done
+
+  if [[ -f "${HOME}/.ssh/known_hosts.old" ]]; then
+    "${rm_bin}" -f "${HOME}/.ssh/known_hosts.old";
+  fi
+  builtin return 0;
+}
