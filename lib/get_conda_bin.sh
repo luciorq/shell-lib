8237d52705506929c1772da415aff27afc590c63
luciorq <luciorqueiroz@gmail.com>
2023-03-27 15:04:58 -0400

    feat: create conda_priv_fun

--- lib/get_conda_bin.sh
+++ lib/get_conda_bin.sh
@@ -3,22 +3,42 @@
 # Return conda executable based on preference
 function get_conda_bin () {
   local conda_bin;
-  conda_bin="$(which_bin 'mamba')";
+  conda_bin="$(which_bin 'micromamba')";
   if [[ -z ${conda_bin} ]]; then
-    conda_bin="$(which_bin 'micromamba')";
+    conda_bin="$(which_bin 'mamba')";
   fi
   if [[ -z ${conda_bin} ]]; then
     conda_bin="$(which_bin 'conda')";
   fi
   if [[ -z ${conda_bin} ]] && \
     [[ "$(LC_ALL=C builtin type -t '__install_app')" =~ function ]]; then
-    __install_app 'mamba';
-    conda_bin="$(which_bin 'mamba')";
+    __install_app 'micromamba';
+    conda_bin="$(which_bin 'micromamba')";
   fi
   if [[ -z ${conda_bin} ]]; then
-    exit_fun 'Conda is not available for this system';
+    exit_fun 'conda, mamba, and micromamba are not available for this system';
     return 1;
   fi
   builtin echo -ne "${conda_bin}";
   return 0;
+}
+
+function conda_priv_fun () {
+  local conda_bin;
+  local env_name;
+  local conda_env_exports;
+  conda_bin="$(get_conda_bin)";
+  if [[ ${1} =~ activate ]]; then
+    env_name="${2-}";
+    if [[ "${conda_bin}" =~ micromamba$ ]]; then
+      conda_env_exports="$("${conda_bin}" shell --shell bash activate "${env_name}")";
+    else
+      conda_env_exports="$("${conda_bin}" shell.posix activate "${env_name}")";
+    fi
+    builtin eval "${conda_env_exports}";
+    builtin hash -r;
+    builtin return 0;
+  fi
+  "${conda_bin}" "${@}";
+  builtin return 0;
 }
