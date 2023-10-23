5f82a0e6ebcb080803ed005a3c59815ceb468d87
luciorq <luciorqueiroz@gmail.com>
2023-10-23 11:51:35 -0400

    feat: create docker_get_available_memory

---
+++ lib/docker_funs.sh
@@ -0,0 +1,15 @@
+# Functions and Wrappers around Docker CLI commands
+
+# Return available memory inside a docker container
+function docker_get_available_memory () {
+  builtin local docker_bin;
+  docker_bin="$(require 'docker')";
+  if [[ -z ${docker_bin} ]]; then
+    builtin return 1;
+  fi
+  "${docker_bin}" run \
+    --rm \
+    "debian:bullseye-slim" \
+    bash -c 'numfmt --to iec $(echo $(($(getconf _PHYS_PAGES) * $(getconf PAGE_SIZE))))';
+  builtin return 0;
+}
