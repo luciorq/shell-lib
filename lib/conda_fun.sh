ae4cdce84e4258b83c25e59f9691791bc60dc647
luciorq <luciorqueiroz@gmail.com>
2023-10-28 10:48:23 -0400

    feat: create conda_run

---
+++ lib/conda_fun.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+function conda_list_envs () {
+  conda_priv_fun 'env' 'list' -q --json;
+}
+
+function conda_env_exists () {
+  local env_name;
+  local jq_bin;
+  local grep_bin;
+  local list_envs_res;
+  jq_bin=$(require 'jq');
+  grep_bin=$(require 'grep');
+  env_name="${1:-}";
+  if [[ -z ${env_name} ]]; then
+    exit_fun 'conda_env_exists: `env_name` not provided';
+    builtin return 1;
+  fi
+  list_envs_res="$(\
+    conda_list_envs \
+      | "${jq_bin}" -r '.envs[]' \
+      | "${grep_bin}" "${env_name}$"\
+  )";
+  builtin echo -ne "${list_envs_res}";
+  builtin return 0;
+}
+
+function conda_run () {
+  local env_name;
+  # local conda_bin;
+  local conda_env_exists_res;
+  env_name="${1:-}";
+  if [[ -z ${env_name} ]]; then
+    exit_fun 'conda_run: `env_name` not provided';
+    builtin return 1;
+  fi
+  conda_env_exists_res="$(conda_env_exists "${env_name}")";
+  if [[ -z ${conda_env_exists_res} ]]; then
+    exit_fun "conda_run: Environment \`${env_name}\` does not exist";
+    builtin return 1;
+  fi
+  conda_priv_fun run -n "${env_name}" "${@:2}";
+  builtin return 0;
+}
+
+function conda_create_env () {
+  builtin local env_name;
+  builtin local pkgs_str;
+  builtin local channels_str;
+  builtin local conda_env_exists_res;
+  builtin local platform_str;
+  env_name="${1:-}";
+  pkgs_str="${2:-}";
+  channels_str="${3:-}";
+  platform_str="${4:-}";
+  conda_env_exists_res="$(conda_env_exists "${env_name}")";
+  if [[ -n ${conda_env_exists_res} ]]; then
+    builtin return 0;
+  fi
+  if [[ -z ${pkgs_str} ]]; then
+    exit_fun 'conda_create_env: Packages not provided';
+    builtin return 1;
+  fi
+  if [[ -z ${platform_str} ]]; then
+    platform_str="$(get_conda_platform)";
+  fi
+  # --platform "${platform_str}"
+  conda_priv_fun create -y -n "${env_name}" ${channels_str} ${pkgs_str};
+  builtin return 0;
+}
