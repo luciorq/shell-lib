d0aeb9abbee2e71e9f85b38dc528dfea12e65c1f
luciorq <luciorqueiroz@gmail.com>
2024-06-28 11:41:36 -0400

    feat: create gen_passwd function

---
+++ lib/gen_password.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Generate Random Password
+function gen_passwd() {
+  \builtin local mkpw_bin
+  \builtin local gopass_bin
+  \builtin local head_bin
+  \builtin local pw_str
+  mkpw_bin="$(which_bin 'mkpasswd')"
+  gopass_bin="$(which_bin 'gopass')"
+  head_bin="$(require 'head')"
+
+  if [[ -z ${gopass_bin} ]] && [[ -z ${mkpw_bin} ]]; then
+    \builtin echo -ne 'Error: At least one password generator method should be available'
+    \builtin return 1
+  fi
+
+  if [[ -z ${gopass_bin} ]]; then
+    pw_str="$("${mkpw_bin}" ...)"
+  else
+    pw_str="$(
+      "${gopass_bin}" pwgen --symbols --one-per-line 28 |
+        "${head_bin}" -1
+    )"
+  fi
+
+  if [[ -z ${pw_str} ]]; then
+    exit_fun 'Error: Password String can not be empty'
+    \builtin return 1
+  fi
+
+  \builtin echo -ne "${pw_str}"
+  \builtin return 0
+}
