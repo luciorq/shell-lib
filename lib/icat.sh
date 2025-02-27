eed43f7f3c82600c4cf53fba0388ad868927dacd
luciorq <luciorqueiroz@gmail.com>
2025-02-27 17:50:05 -0500

    chore: lint

--- lib/icat.sh
+++ lib/icat.sh
@@ -4,13 +4,13 @@
 # + if kitty API is available use
 # + icat kitten
 function icat () {
-  local bash_bin;
-  local kitty_bin;
+  \builtin local bash_bin;
+  \builtin local kitty_bin;
   bash_bin="$(which_bin 'bash')";
   kitty_bin="$(require 'kitty')";
   if [[ -z ${kitty_bin} ]]; then
-    return 1;
+    \builtin return 1;
   fi
   "${bash_bin}" "${kitty_bin}" +kitten icat "${@}";
-  return 0;
+  \builtin return 0;
 }
