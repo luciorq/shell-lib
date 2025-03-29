ee9ab145638b9ba62bd742844e656babb12831a4
luciorq <luciorqueiroz@gmail.com>
2025-03-29 09:47:59 -0400

    feat: user_name argument in get_default_shell

--- lib/get_default_shell.sh
+++ lib/get_default_shell.sh
@@ -1,21 +1,30 @@
 #!/usr/bin/env bash
 
 function get_default_shell () {
+  \builtin local _usage;
+  _usage="usage: ${0} [<USER_NAME>]";
+  \builtin unset _usage;
+  \builtin local user_name;
   \builtin local dscl_bin;
   \builtin local getent_bin;
+  user_name="${1:-${USER}}";
+  if [[ -z "${user_name}" ]]; then
+    exit_fun 'User name is empty';
+    \builtin return 1;
+  fi
   dscl_bin="$(which_bin dscl)";
   getent_bin="$(which_bin getent)";
   # MacOS
   if [[ $(get_os_type) =~ 'darwin' ]] && [[ -n "${dscl_bin}" ]]; then
-    dscl . -read "${HOME}" UserShell \
+    dscl . -read "/Users/${user_name}" UserShell \
       | awk -F': ' '/UserShell/ {print $2}' \
       | sed 's/^[ \t]*//;s/[ \t]*$//';
   # getent respects values in LDAP
   elif [[ -n "${getent_bin}" ]]; then
     "${getent_bin}" passwd \
-      | awk -F: -v user="${USER}" '$1 == user {print $NF}';
+      | awk -F: -v user="${user_name}" '$1 == user {print $NF}';
   else
-    grep "^${USER}:" /etc/passwd \
+    grep "^${user_name}:" /etc/passwd \
       | cut -d: -f7;
   fi
   \builtin return 0;
