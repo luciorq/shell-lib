8ad8de94ddf0ba787ea9444cbc7ed6c2682e0f37
luciorq <luciorqueiroz@gmail.com>
2025-06-25 09:51:21 -0400

    feat: add glow detection for dfh

--- lib/dfh.sh
+++ lib/dfh.sh
@@ -6,6 +6,7 @@ function dfh () {
   \builtin local grep_bin;
   \builtin local sed_bin;
   \builtin local column_bin;
+  \builtin local glow_bin;
   \builtin local header_str;
   \builtin local align_str;
   \builtin local body_str;
@@ -16,6 +17,7 @@ function dfh () {
   grep_bin="$(which_bin 'grep')";
   sed_bin="$(which_bin 'sed')";
   column_bin="$(which_bin 'column')";
+  glow_bin="$(which_bin 'glow')";
   # NOTE: @luciorq On my remote servers I always mount
   # + data directories at `/data/{ hostname }` also `/home`
   # + is usually a NFS endpoint.
@@ -29,13 +31,30 @@ function dfh () {
       | "${grep_bin}" -v '^Filesystem.*on$' \
       | "${sed_bin}" -r 's/\s+/,/g'
   )";
+
+  if [[ -n ${glow_bin} ]]; then
+    # NOTE: @luciorq I use glow to render markdown
+    # + in the terminal, but it is not installed by
+    # + default on all systems.
+    # + `pixi global install glow-md`
   CLICOLOR_FORCE=1 \
     builtin echo -ne \
+      "${header_str}${align_str}${body_str}\n" \
+    | bat_fun -l csv -pp --color=never \
+    | "${sed_bin}" 's/,/ | /g' \
+    | "${sed_bin}" 's/^/| /g' \
+    | "${sed_bin}" 's/$/ |/g' \
+    | "${column_bin}" -t \
+    | "${glow_bin}" -w 120;
+  else
+    CLICOLOR_FORCE=1 \
+      builtin echo -ne \
       "${header_str}${align_str}${body_str}\n" \
     | bat_fun -l csv -pp --color=always \
     | "${sed_bin}" 's/,/ | /g' \
     | "${sed_bin}" 's/^/| /g' \
     | "${sed_bin}" 's/$/ |/g' \
     | "${column_bin}" -t;
+  fi
   \builtin return 0;
 }
